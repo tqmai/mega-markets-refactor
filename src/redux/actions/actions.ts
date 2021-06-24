c2c10f6d2960e87314a56453b864b4c8555b254f
Timothy Mai <timothy.mai@pacificlife.com>
2021-06-24 15:06:00 -0700

    refactor actions to ts

--- src/redux/actions/actions.ts
+++ src/redux/actions/actions.ts
@@ -12,23 +12,23 @@
 // import actionType constants
 import * as types from "../constants/actionTypes";
 
-export const addCard = (marketId) => ({
+export const addCard = (marketId: number) => ({
   type: types.ADD_CARD,
   payload: marketId,
 });
 
 // add more action creators
-export const addMarket = (location) => ({
+export const addMarket = (location: string) => ({
   type: types.ADD_MARKET,
   payload: location,
 });
 
-export const setNewLocation = (location) => ({
+export const setNewLocation = (location: string) => ({
   type: types.SET_NEW_LOCATION,
   payload: location,
 });
 
-export const deleteCard = (marketId) => ({
+export const deleteCard = (marketId: number) => ({
   type: types.DELETE_CARD,
   payload: marketId,
 });
