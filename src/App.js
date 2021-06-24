7ee27fab1272639b19d286af56537c02bf59ad8a
Timothy Mai <timothy.mai@pacificlife.com>
2021-06-23 17:15:20 -0700

    update some colors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import styled from "styled-components";
 import TotalsDisplay from "./components/TotalsDisplay";
 import MarketsContainer from "./containers/MarketsContainer";
 import { Typography } from "./components/atoms/typography";
 import { Grid } from "./components/atoms/grid";
+import { Colors } from "./styles/tokens";
+
+const StyledTypography = styled(Typography)`
+  color: ${Colors.primary};
+`;
 
 const App = () => (
   <Grid container justify="center">
     <Grid item sm={12}>
-      <Typography variant="h1" align="center" display="block">
+      <StyledTypography variant="h1" align="center" display="block">
         MegaMarket Loyalty Cards
-      </Typography>
+      </StyledTypography>
       <TotalsDisplay />
       <MarketsContainer />
     </Grid>
